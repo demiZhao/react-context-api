0ae8f6661bd5b954623e4cecea0c575447ea86c4
demi zhao <demizhao@demis-MacBook-Pro.local>
2020-05-02 17:50:40 -0400

    Updates

---
+++ precache-manifest.f430f2a4d8ab02370e882a2072a3d5fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53081c3db4f20c9948e4",
+    "url": "/react-context-api/static/js/main.41e35ef3.chunk.js"
+  },
+  {
+    "revision": "0084e4d665b13de5271d",
+    "url": "/react-context-api/static/js/runtime~main.02aa04ec.js"
+  },
+  {
+    "revision": "25b7d7de2b48782d672f",
+    "url": "/react-context-api/static/css/2.ac17ffd6.chunk.css"
+  },
+  {
+    "revision": "25b7d7de2b48782d672f",
+    "url": "/react-context-api/static/js/2.7de07ac4.chunk.js"
+  },
+  {
+    "revision": "88dc7ecd5fe2d7f42ac61d45af4610e5",
+    "url": "/react-context-api/index.html"
+  }
+];
